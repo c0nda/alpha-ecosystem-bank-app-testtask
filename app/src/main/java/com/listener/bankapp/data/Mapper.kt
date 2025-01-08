1f7d2da4d4ed4e60769447b10c1e1d003f293651
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2025-01-08 20:54:13 +0400

    Added new extensions

--- app/src/main/java/com/listener/bankapp/data/Mapper.kt
+++ app/src/main/java/com/listener/bankapp/data/Mapper.kt
@@ -1,5 +1,7 @@
 package com.listener.bankapp.data
 
+import com.listener.bankapp.domain.models.BankCardHistory
+import com.listener.bankapp.data.local.entity.BankCardEnt
 import com.listener.bankapp.data.remote.dto.BankCardDTO
 import com.listener.bankapp.data.remote.dto.BankDTO
 import com.listener.bankapp.data.remote.dto.CountryDTO
@@ -36,4 +38,50 @@ fun CountryDTO.toDomain() = Country(
     emoji = emoji,
     latitude = latitude,
     longitude = longitude
+)
+
+fun BankCardEnt.toDomain() = BankCardHistory(
+    bin = bin,
+    errorMessage = errorMessage,
+    bankCard = BankCard(
+        number = Number(
+            length = numberLength,
+            luhn = numberLuhn
+        ),
+        scheme = scheme,
+        type = type,
+        brand = brand,
+        prepaid = prepaid,
+        country = Country(
+            name = countryName,
+            emoji = countryEmoji,
+            latitude = countryLatitude,
+            longitude = countryLongitude
+        ),
+        bank = Bank(
+            name = bankName,
+            url = bankUrl,
+            phone = bankPhone,
+            city = bankCity
+        )
+    )
+)
+
+fun BankCard.toEntity(bin: Long, errorMessage: String? = null) = BankCardEnt(
+    bin = bin,
+    errorMessage = errorMessage,
+    numberLength = number?.length,
+    numberLuhn = number?.luhn,
+    scheme = scheme,
+    type = type,
+    brand = brand,
+    prepaid = prepaid,
+    countryName = country?.name,
+    countryEmoji = country?.emoji,
+    countryLatitude = country?.latitude,
+    countryLongitude = country?.longitude,
+    bankName = bank?.name,
+    bankUrl = bank?.url,
+    bankPhone = bank?.phone,
+    bankCity = bank?.city
 )
