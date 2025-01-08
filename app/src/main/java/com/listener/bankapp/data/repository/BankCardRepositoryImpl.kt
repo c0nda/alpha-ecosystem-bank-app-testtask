a6ac94aa5be3cce16ca3b6de4d6d01a5f135f566
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2025-01-08 20:53:13 +0400

    Added local data source

--- app/src/main/java/com/listener/bankapp/data/repository/BankCardRepositoryImpl.kt
+++ app/src/main/java/com/listener/bankapp/data/repository/BankCardRepositoryImpl.kt
@@ -1,17 +1,22 @@
 package com.listener.bankapp.data.repository
 
+import com.listener.bankapp.data.local.LocalDataSource
 import com.listener.bankapp.data.remote.RemoteDataSource
 import com.listener.bankapp.domain.models.BankCard
 import com.listener.bankapp.domain.repository.BankCardRepository
 import com.listener.bankapp.utils.Request
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 class BankCardRepositoryImpl @Inject constructor(
-    private val remoteDataSource: RemoteDataSource
+    private val remoteDataSource: RemoteDataSource,
+    private val localDataSource: LocalDataSource
 ) : BankCardRepository {
 
     override suspend fun loadBankCardInfo(bin: Long): Flow<Request<BankCard>> {
-        return remoteDataSource.loadBankCardInfoByBin(bin)
+        return remoteDataSource.loadBankCardInfoByBin(bin).onEach { request ->
+            localDataSource.insertRequest(bin, request)
+        }
     }
 }
